375054f904c3e3be1b1161440e7b700a763462c4
JakeGrin <callmedestroyer3@gmail.com>
2023-05-07 13:36:41 +0200

    nativeQuery please,yong man on repo etc

--- src/main/java/org/example/repository/BookRepository.java
+++ src/main/java/org/example/repository/BookRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    @Query(value = "select b.* from book b join " +
-            "book_author ba on b.id = ba.book_id join " +
-            "author a on ba.author_id = a.id " +
-            "where a.gender_author = :gender")
+    @Query(value = " select b.* from book b join " +
+            " book_author ba on b.id = ba.book_id " +
+            " join " +
+            " author a on ba.author_id = a.id " +
+            " where a.gender_author = :gender",nativeQuery = true)
     List<Book> findAllByAuthorGender(String gender);
 }
