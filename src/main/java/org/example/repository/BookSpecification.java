04f032728b9012b0e31203ac3bea9dded97ab83f
JakeGrin <callmedestroyer3@gmail.com>
2023-05-18 18:44:38 +0200

    gender fighting final round

--- src/main/java/org/example/repository/BookSpecification.java
+++ src/main/java/org/example/repository/BookSpecification.java
@@ -2,48 +2,63 @@ package org.example.repository;
 
 import org.example.entity.*;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.expression.spel.ast.Selection;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.time.LocalDate;
+import java.util.List;
 
 public class BookSpecification {
-
-
+    static EntityManager em;
 
     public static Specification<Book> nameOfBookEqual(String nameOfBook) {
         return (book, cq, cb) -> cb.equal(book.get("nameOfBook"), nameOfBook);
     }
 
     public static Specification<Book> dateOfPublicationEqual(LocalDate dateOfPublication) {
-        return (book, cq, cb) -> cb.equal(book.get("dateOfPublication"),   dateOfPublication );
+        return (book, cq, cb) -> cb.equal(book.get("dateOfPublication"), dateOfPublication);
     }
+
     public static Specification<Book> publishingHouseEqual(String publishingHouse) {
-        return (book, cq, cb) -> cb.equal(book.get("publishingHouse"),  publishingHouse );
-    }
-
-    public static Specification<Book> genderEqual(String genderAuthor) {
-        return (book, cq, cb) -> {
-            Root<BookAuthor> root = cq.from(BookAuthor.class);
-//            root.join("author");
-//            root.join("book");
-            Path<Object> objectPath = root.get(BookAuthor_.ID);
-            Path<Object> genderAuthor1 = root.get(BookAuthor_.AUTHOR).get("genderAuthor");
-            cq.where(cb.equal(root.get("book_id"),root.get(BookAuthor_.BOOK).get("id")),
-                    cb.equal(root.get("author_id"),root.get(BookAuthor_.AUTHOR).get("id")));
-
-//            Join<BookAuthor, Book> bookAuthorBook = bookAuthorRoot.join("book");
-//            Join<BookAuthor, Author> bookAuthorAuthor = bookAuthorBook.join("author");
-//            return cb.equal(bookAuthorJoin.get("genderAuthor"), genderAuthor);
-//              cq.where(cb.and(cb.equal(bookAuthorBook.get("book"))),
-//                    cb.equal(bookAuthorAuthor.get("author")));
-//              return cb.equal(bookAuthorAuthor.get("genderAuthor"),genderAuthor)
-//         return cb.equal(bookAuthorRoot.join(Book_.ID).join(Author_.ID).get("genderAuthor"), genderAuthor);
-//            cq.where(cb.equal(root.get(BookAuthor_.AUTHOR).get("genderAuthor"), genderAuthor));
-            return cb.equal(genderAuthor1,genderAuthor);
-
-
-
-
-        };
+        return (book, cq, cb) -> cb.equal(book.get("publishingHouse"), publishingHouse);
     }
 }
+//
+//    public static Specification<Book> genderEqual(String genderAuthor) {
+//
+//            CriteriaBuilder cb = em.getCriteriaBuilder();
+//            CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+//        Root<Book> book = cq.from(Book.class);
+//        Root<BookAuthor> bookAuthorRoot = cq.from(BookAuthor.class);
+//            Join<BookAuthor, Author> author = bookAuthorRoot.join("author");
+//            Join<BookAuthor, Book> bookRoot = bookAuthorRoot.join("book");
+//
+//                Predicate equalityAuthorPredicate = cb.equal(
+//                        bookAuthorRoot.get("author_id"),
+//                        author.get("id")
+//                );
+//
+//                Predicate equalityBookPredicate = cb.equal(
+//                        bookAuthorRoot.get("book_id"),
+//                        book.get("id")
+//                );
+//
+//        Predicate equalityGenderPredicate = cb.equal(
+//                author.get("genderAuthor"),
+//                genderAuthor);
+//
+//            cq.where(cb.and(equalityAuthorPredicate,equalityBookPredicate,equalityGenderPredicate));
+//            cb.createQuery();
+//            List<Book> result = em.createQuery(cq).getResultList();
+//
+//            return result;
+
+
+
+//
+//        };
+//    }
+//}
--- src/main/java/org/example/service/impl/BookServiceImpl.java
+++ src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,20 +1,30 @@
 package org.example.service.impl;
 
+import org.example.entity.Author;
 import org.example.entity.Book;
+import org.example.entity.BookAuthor;
 import org.example.repository.AuthorRepository;
 import org.example.repository.BookRepository;
 import org.example.repository.BookSpecification;
 import org.example.service.api.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Service
 public class BookServiceImpl implements BookService {
+    @Autowired
+    private EntityManager em;
     @Autowired
     private BookRepository bookRepository;
 
@@ -72,10 +82,36 @@ public class BookServiceImpl implements BookService {
 //    }
 
     @Override
-    public List<Book> getBooksWithGender(String search) {
-//        List<Book> all = bookRepository.findAll();
+    public List<Book> getBooksWithGender(String genderAuthor) {
 
-        return bookRepository.findAll(BookSpecification.genderEqual(search));
+
+
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+            Root<Book> book = cq.from(Book.class);
+            Root<BookAuthor> bookAuthorRoot = cq.from(BookAuthor.class);
+            Join<BookAuthor, Author> author = bookAuthorRoot.join("author");
+//            Join<BookAuthor, Book> bookRoot = bookAuthorRoot.join("book");
+
+            Predicate equalityAuthorPredicate = cb.equal(
+                    bookAuthorRoot.get("author_id"),
+                    author.get("id")
+            );
+
+            Predicate equalityBookPredicate = cb.equal(
+                    bookAuthorRoot.get("book_id"),
+                    book.get("id")
+            );
+
+            Predicate equalityGenderPredicate = cb.equal(
+                    author.get("genderAuthor"),
+                    genderAuthor);
+
+            cq.where(cb.and(equalityAuthorPredicate,equalityBookPredicate,equalityGenderPredicate));
+            cq.select(book);
+            List<Book> result = em.createQuery(cq).getResultList();
+
+            return result;
     }
 
     @Override
